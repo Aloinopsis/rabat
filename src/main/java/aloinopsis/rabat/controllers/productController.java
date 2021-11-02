b6341fa563f6980defb6043de9e52ca46088ea33
Aloinopsis <darekgumula@gmail.com>
2021-11-02 16:52:57 +0100

    ProductNotFoundException

--- src/main/java/aloinopsis/rabat/controllers/productController.java
+++ src/main/java/aloinopsis/rabat/controllers/productController.java
@@ -1,7 +1,6 @@
 package aloinopsis.rabat.controllers;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import aloinopsis.rabat.exceptions.ProductNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,14 +28,11 @@ public class productController {
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<String> getProduct(@PathVariable("id") Integer id) {
+    public String getProduct(@PathVariable Integer id) {
         if (id > products.size() || id < 1) {
-            return ResponseEntity
-                    .status(HttpStatus.NOT_FOUND)
-                    .body("Produkt nie odnaleziony");
+            throw new ProductNotFoundException();
         }
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(products.get(id - 1));
+
+        return products.get(id - 1);
     }
 }
